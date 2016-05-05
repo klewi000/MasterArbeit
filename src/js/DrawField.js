b8f1572c0f16eec30adf56dc174cb9b51e84cff7
klewi000 <klemens.w@gmx.at>
2016-05-05 17:15:22 +0200

    move Places

--- src/js/DrawField.js
+++ src/js/DrawField.js
@@ -1,35 +1,56 @@
 class DrawField {
-
     static drawPlaces(placeList) {
-        STAGE = new createjs.Stage("vizTSP");
+        stage = new createjs.Stage("vizTSP");
         for (let i = 0; i < placeList.length; i++) {
-            // console.log("Place: " + i + " posX: " + placeList[i].x + " posY: " + placeList[i].y);
             var circle = new createjs.Shape();
-            circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5);
+            circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 8);
             circle.x = placeList[i].x;
             circle.y = placeList[i].y;
-            STAGE.addChild(circle);
-            STAGE.update();
+            stage.addChild(circle);
+            stage.update();
+
+            circle.on("mousedown", function (evt) {
+                if (movePlaces) {
+                    evt.target.graphics.clear().beginFill("#Blue").drawCircle(0, 0, 8);
+                    stage.update();
+                }
+            });
+
+            circle.on("pressmove", function (evt) {
+                if (movePlaces) {
+                    evt.target.x = evt.stageX;
+                    evt.target.y = evt.stageY;
+                    stage.update();
+                }
+            });
+            circle.on("pressup", function (evt) {
+                if (movePlaces) {
+                    placeList[i].x = evt.stageX;
+                    placeList[i].y = evt.stageY;
+                    evt.target.graphics.clear().beginFill("DeepSkyBlue").drawCircle(0, 0, 8);
+                    stage.update();
+                }
+            });
         }
     }
 
-    static drawTrack(track, placeList){
+    static drawTrack(track, placeList, first = true) {
+        console.log("draw track...");
         DrawField.drawPlaces(placeList);
         var line = new createjs.Shape();
         line.graphics.setStrokeStyle(3);
-        var color  = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        var color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
         line.graphics.beginStroke(color, 0.01);
         line.alpha = 0.5;
 
         line.graphics.moveTo(placeList[track.placeOrder[0]].x, placeList[track.placeOrder[0]].y);
-        for(let i = 1; i < track.placeOrder.length; i++){
+        for (let i = 1; i < track.placeOrder.length; i++) {
             line.graphics.lineTo(placeList[track.placeOrder[i]].x, placeList[track.placeOrder[i]].y);
         }
         line.graphics.lineTo(placeList[track.placeOrder[0]].x, placeList[track.placeOrder[0]].y);
-
         line.graphics.endStroke();
-        STAGE.addChild(line);
-        STAGE.update();
+        stage.addChild(line);
+        stage.update();
     }
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,46 @@
 (function () {
 
-    STAGE = new createjs.Stage("vizTSP");
-    var trackList;
+    stage = new createjs.Stage("vizTSP");
+    movePlaces = true;
 
     //Button generate Places
-    $('#generatePlaces').click(function(){
+    $('#generatePlaces').click(function () {
         var numOfPlaces = $('#amountPlaces').val();
+        //globale variable placeList!!!
         placeList = new PlaceList(numOfPlaces);
+        drawField = new DrawField();
         DrawField.drawPlaces(placeList);
         $('#generateTracks').prop('disabled', false);
+        movePlaces = true;
     });
 
     //Button generate Tracks
-    $('#generateTracks').click(function(){
+    $('#generateTracks').click(function () {
         var numOfTracks = $('#amountTracks').val();
+        //globale variable trackList!!!
         trackList = new TrackList(numOfTracks, placeList.length);
-        $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength *100)/100);
+        $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
         DrawField.drawTrack(trackList.trackList[0], placeList);
         $('#clcTspOne').prop('disabled', false);
         $('#clcTspAuto').prop('disabled', false);
+        movePlaces = false;
     });
 
     //Button calc TSP one step
-    $('#clcTspOne').click(function(){
+    $('#clcTspOne').click(function () {
         trackList.mutateTrackList();
         $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
         DrawField.drawTrack(trackList.trackList[0], placeList);
+
     });
 
-    $('#clcTspAuto').click(function(){
-        // var numGenerations = $('#amountGenerations').val();
-        // for(let i = 0; i < numGenerations; i++){
-        //     trackList.mutateTrackList();
-        // }
-        for(let j = 0; j < 10; j++){
-            for(let i = 0; i < 100; i++){
+    $('#clcTspAuto').click(function () {
+        var numGenerations = $('#amountGenerations').val();
+        for(let i = 0; i < numGenerations; i++){
                 trackList.mutateTrackList();
-                console.log("mutate");
-            }
-            console.log("draw");
-            $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
-            DrawField.drawTrack(trackList.trackList[0], placeList);
-            window.setTimeout(2000);
+                $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
+                DrawField.drawTrack(trackList.trackList[0], placeList);
         }
-
     });
+
 })();
