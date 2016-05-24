4c51d6d5d05727145bc175f28b1f1d636fc7c60b
winterer <mario.winterer+github@gmail.com>
2016-05-24 22:30:34 +0200

    Redesign (#1)

--- src/js/TrackList.js
+++ src/js/TrackList.js
@@ -1,46 +1,42 @@
 class TrackList {
-    constructor(numOfTracks, numOfPlaces) {
-        this.trackList = [];
+    constructor(places, numOfTracks) {
+        this.places = places;
+        this.tracks = [];
 
         for (let i = 0; i < numOfTracks; i++) {
-            this.trackList.push(new Track(numOfPlaces));
-            this.trackList[i].trackLength = this.calcTrackLength(this.trackList[i]);
+            this.tracks.push(new Track(places, places.length));
         }
+
         this.sortTrackList();
     }
 
-    calcTrackLength(track) {
-        var trackLength = 0;
-        for (let i = 0; i < track.placeOrder.length - 1; i++) {
-            trackLength += Math.sqrt(
-                Math.pow(placeList[track.placeOrder[i + 1]].x - placeList[track.placeOrder[i]].x, 2) +
-                Math.pow(placeList[track.placeOrder[i + 1]].y - placeList[track.placeOrder[i]].y, 2)
-            );
-        }
-        trackLength += Math.sqrt(
-            Math.pow(placeList[track.placeOrder[track.placeOrder.length - 1]].x - placeList[track.placeOrder[0]].x, 2) +
-            Math.pow(placeList[track.placeOrder[track.placeOrder.length - 1]].y - placeList[track.placeOrder[0]].y, 2)
-        );
-        return trackLength;
+    get length() {
+        return this.tracks.length;
+    }
+
+    clone() {
+        var list = new TrackList(this.places, 0);
+        list.tracks = this.tracks.slice();
+        return list;
     }
 
     sortTrackList() {
-        this.trackList.sort(function (a, b) {
-            return parseFloat(a.trackLength) - parseFloat(b.trackLength);
-        })
+        this.tracks.sort(function (a, b) {
+            return a.distance - b.distance;
+        });
     }
 
     mutateTrackList() {
-        var childSolCand = this.trackList;
-        for (let i = 0; i < this.trackList.length; i++) {
-            var oldPlaceOrder = this.trackList[i].placeOrder.slice();
-            var oldTrackLength = this.trackList[i].trackLength;
-            this.trackList[i].mutateTrack();
-            this.trackList[i].trackLength = this.calcTrackLength(this.trackList[i]);
+        var childSolCand = this.tracks;
+        for (let i = 0; i < this.tracks.length; i++) {
+            var oldPlaceOrder = this.tracks[i].placeOrder.slice();
+            var oldTrackLength = this.tracks[i].trackLength;
+            this.tracks[i].mutateTrack();
+            this.tracks[i].trackLength = this.calcTrackLength(this.tracks[i]);
 
-            if (oldTrackLength < this.trackList[i].trackLength) {
-                this.trackList[i].placeOrder = oldPlaceOrder.slice();
-                this.trackList[i].trackLength = oldTrackLength;
+            if (oldTrackLength < this.tracks[i].trackLength) {
+                this.tracks[i].placeOrder = oldPlaceOrder.slice();
+                this.tracks[i].trackLength = oldTrackLength;
             }
         }
         this.sortTrackList();
--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,123 @@
 (function () {
 
-    stage = new createjs.Stage("vizTSP");
+    // TODO: move to DrawField???
     movePlaces = true;
 
+    var canvasWidth = $('#vizTSP').width();
+    var canvasHeight = $('#vizTSP').height();
+
+    stage = new createjs.Stage("vizTSP");
+    drawField = new DrawField(stage);
+    generations = [];
+    
     //Button generate Places
     $('#generatePlaces').click(function () {
         var numOfPlaces = $('#amountPlaces').val();
-        //globale variable placeList!!!
-        placeList = new PlaceList(numOfPlaces);
-        drawField = new DrawField();
-        DrawField.drawPlaces(placeList);
-        DrawField.drawPlaces(placeList);
+        // global place list variable
+        placeList = new PlaceList(canvasWidth, canvasHeight, numOfPlaces);
+        drawField.removeAllTracks();
+        drawField.setPlaces(placeList);
         $('#generateTracks').prop('disabled', false);
-        movePlaces = true;
+        // movePlaces = true;
     });
 
     //Button generate Tracks
     $('#generateTracks').click(function () {
         var numOfTracks = $('#amountTracks').val();
-        //globale variable trackList!!!
-        trackList = new TrackList(numOfTracks, placeList.length);
-        $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
-        DrawField.drawTrack(trackList.trackList[0], placeList);
+        
+        var initialGen = [];
+        for(let i = 0; i < numOfTracks; ++i) {
+            initialGen.push(new Track(placeList, placeList.length));
+        }
+        initialGen.sort(function (a, b) { return a.distance - b.distance });
+
+        var bestTrack = initialGen[0];
+        $('#outputTrackLength').text(Math.round(bestTrack.distance * 100) / 100);
+        
+        drawField.removeAllTracks();
+        drawField.addTrack(bestTrack);
+        //for(let i = 0; i < trackList.tracks.length; ++i) {
+            //drawField.addTrack(trackList.tracks[i]);
+        //}
+        
         $('#clcTspOne').prop('disabled', false);
         $('#clcTspAuto').prop('disabled', false);
         movePlaces = false;
+
+        // add initial generation to generations array
+        generations = [];
+        generations.push(initialGen);
     });
 
     //Button calc TSP one step
     $('#clcTspOne').click(function () {
-        trackList.mutateTrackList();
-        $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
-        DrawField.drawTrack(trackList.trackList[0], placeList);
-
+        computeNextGeneration();
     });
+
     //Button calc TSP Auto
     $('#clcTspAuto').click(function () {
         var numGenerations = $('#amountGenerations').val();
-        for (let i = 0; i < numGenerations; i++) {
-            trackList.mutateTrackList();
-            $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
-            DrawField.drawTrack(trackList.trackList[0], placeList);
-        }
+        
+        generations.splice(0, generations.length - 1);
+        var intervalId = window.setInterval(function() {
+            while(generations.length < numGenerations) {
+                if (computeNextGeneration() < 0) {
+                    // we've found a new better solution
+                    return;
+                }
+            }
+            window.clearInterval(intervalId);
+        }, 100);
     });
+
+    function computeNextGeneration() {
+        var gen = generations[generations.length - 1];
+        var bestTrack = gen[0];
+
+        var nextGen = createNewGeneration(gen);
+        var newBestTrack = nextGen[0];
+
+        generations.push(nextGen);
+        
+        $('#outputTrackLength').text(Math.round(newBestTrack.distance * 100) / 100);
+        drawField.removeTrack(bestTrack); // remove old best gen
+        drawField.addTrack(newBestTrack);
+
+        return newBestTrack.distance - bestTrack.distance;
+    }
+
+    function createNewGeneration(gen) {
+        /* create new generation from gen */
+        var nextGen = [];
+
+        for(let i = 0; i < gen.length; ++i) {
+            var old = gen[i];
+            var t = old.clone().mutate();
+            if (t.distance < old.distance) {
+                nextGen.push(t);    
+            } else {
+                nextGen.push(old.clone());
+            }
+        }
+
+        /*
+        var m = 5;
+        for(let i = 0; i < m; ++i) {
+            // take m best individuals without any modification
+            nextGen.push(gen[i].clone());
+        }
+
+        for(let i = m + 1; i < gen.length; ++i) {
+            // take any random individual
+            var idx = Math.floor(Math.random() * gen.length);
+            var old = gen[idx];
+            var t = old.clone().mutate();
+            nextGen.push(t);    
+        }
+        */
+
+        nextGen.sort(function (a, b) { return a.distance - b.distance });
+
+        return nextGen;
+    }
 })();
