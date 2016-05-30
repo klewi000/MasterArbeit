77c4a51ab04c52dbac184982711c675b6bc33de8
klewi000 <klemens.w@gmx.at>
2016-05-31 00:19:17 +0200

    drag&drop, hover

--- src/js/DrawField.js
+++ src/js/DrawField.js
@@ -1,13 +1,14 @@
 class DrawField {
     constructor(stage) {
         this.stage = stage;
+        stage.enableMouseOver(20);
 
         this.places = []; // array of Place
         this.tracks = []; // array of Track
         this.selection = []; // selected place indices
     }
 
-    addTrack(t, alpha = 0.5) {
+    addTrack(t, alpha = 0.8) {
         if (t.shape) {
             throw "Track already added to this draw field!"
         }
@@ -64,7 +65,7 @@ class DrawField {
         this.stage.update();
     }
 
-    setPlaces(places) {
+    setPlaces(places, idx = null) {
         // clear old places if existing
         if (this.placesContainer) {
             this.stage.removeChild(this.placesContainer);
@@ -81,9 +82,16 @@ class DrawField {
                 node.y = places[i].y;
 
                 var circle = new createjs.Shape();
-                circle.graphics.beginFill("#ad4500").drawCircle(0, 0, 5);
-                //circle.x = places[i].x;
-                //circle.y = places[i].y;
+
+                var color = "#ad4500";
+                var dia = 5;
+                if (i == idx) {
+                    //change color and diameter when pressmove place
+                    color = "#e39058";
+                    dia = 7;
+                }
+
+                circle.graphics.beginFill(color).drawCircle(0, 0, dia);
 
                 node.addChild(circle);
                 // node.addChild(new createjs.Text("#" + i));
@@ -103,117 +111,58 @@ class DrawField {
     }
 
     hookListeners(shape, idx) {
-            shape.on("click", function () {
-                var manualmode = $( '#manualmode' ).is( ':checked' );
-                if(manualmode) {
-                    if (this.selection.push(idx) == 2) {
-                        // swap selected places
-                        var idx1 = this.selection[0];
-                        var idx2 = this.selection[1];
-                        var t = this.tracks[0];
-                        if (t) {
-                            this.removeTrack(t);
-                            t.swapPlaces(idx1, idx2);
-                            this.addTrack(t);
-                            t.distance = Number.MAX_VALUE;
-                            $('#outputTrackLength').text(Math.round(t.distance * 100) / 100);
-                        }
-                        this.selection = [];
+        shape.on("click", function () {
+            var manualmode = $('#manualmode').is(':checked');
+            if (manualmode) {
+                if (this.selection.push(idx) == 2) {
+                    // swap selected places
+                    var idx1 = this.selection[0];
+                    var idx2 = this.selection[1];
+                    var t = this.tracks[0];
+                    if (t) {
+                        this.removeTrack(t);
+                        t.swapPlaces(idx1, idx2);
+                        this.addTrack(t);
+                        t.distance = Number.MAX_VALUE;
+                        $('#outputTrackLength').text(Math.round(t.distance * 100) / 100);
                     }
-                }else{
                     this.selection = [];
                 }
-            }, this);
+            } else {
+                this.selection = [];
+            }
+        }, this);
 
-        shape.on("mousedown", function (evt) {
+        shape.on("mouseover", function (evt) {
             if (movePlaces) {
-                // console.log("evt.stage x|y = " + evt.stageX + " | " + evt.stageY);
-                // console.log(this.places[idx]);
-                evt.target.graphics.clear().beginFill("#e39058").drawCircle(0, 0, 5);
+                evt.target.graphics.clear().beginFill("#e39058").drawCircle(0, 0, 7);
                 stage.update();
             }
         }, this);
 
-        shape.on("pressmove", function (evt) {
-            if (movePlaces) {
-                this.places[idx].x = evt.stageX;
-                this.places[idx].y = evt.stageY;
-                drawField.setPlaces(placeList);
-                this.stage.update();
-            }
+        shape.on("mouseout", function (evt) {
+            evt.target.graphics.clear().beginFill("#ad4500").drawCircle(0, 0, 5);
+            stage.update();
         }, this);
 
-        // shape.on("pressup", function (evt) {
-        //     if (movePlaces) {
-        //         placeList[i].x = evt.stageX;
-        //         placeList[i].y = evt.stageY;
-        //         evt.target.graphics.clear().beginFill("#ad4500").drawCircle(0, 0, 5);
-        //         stage.update();
-        //     }
-        // }, this);
+        shape.on("mousedown", function (evt) {
+            if (movePlaces) {
+                evt.target.graphics.clear().beginFill("#e39058").drawCircle(0, 0, 7);
+
+                shape.on("pressmove", function (evt) {
+                    this.places[idx].x = evt.stageX;
+                    this.places[idx].y = evt.stageY;
+                    drawField.setPlaces(placeList, idx);
+                }, this);
+
+                shape.on("pressup", function (evt) {
+                    if (movePlaces) {
+                        drawField.setPlaces(placeList);
+                    }
+                }, this);
+
+                stage.update();
+            }
+        }, this);
     }
-
-    // static drawPlaces(placeList) {
-    //     stage = new createjs.Stage("vizTSP");
-    //     var clickone = true;
-    //     var placeOneX;
-    //     var placeOneY;
-    //     var idxOne;
-
-    //     for (let i = 0; i < placeList.length; i++) {
-    //         var circle = new createjs.Shape();
-    //         circle.graphics.beginFill("#ad4500").drawCircle(0, 0, 5);
-    //         circle.x = placeList[i].x;
-    //         circle.y = placeList[i].y;
-    //         stage.addChild(circle);
-    //         stage.update();
-
-    //         circle.on("mousedown", function (evt) {
-    //             if (movePlaces) {
-    //                 evt.target.graphics.clear().beginFill("#e39058").drawCircle(0, 0, 5);
-    //                 stage.update();
-    //             }
-    //         });
-
-    //         circle.on("pressmove", function (evt) {
-    //             if (movePlaces) {
-    //                 evt.target.x = evt.stageX;
-    //                 evt.target.y = evt.stageY;
-    //                 stage.update();
-    //             }
-    //         });
-    //         circle.on("pressup", function (evt) {
-    //             if (movePlaces) {
-    //                 placeList[i].x = evt.stageX;
-    //                 placeList[i].y = evt.stageY;
-    //                 evt.target.graphics.clear().beginFill("#ad4500").drawCircle(0, 0, 5);
-    //                 stage.update();
-    //             }
-    //         });
-
-    //         circle.on("click", function (evt) {
-    //             console.log("click...");
-    //             var manualmode = $( "#manualmode" ).is( ":checked" );
-    //             if (manualmode && !movePlaces) {
-    //                 if(clickone){
-    //                     idxOne = i;
-    //                     clickone = false;
-    //                 }else{
-    //                     placeOneX = placeList[idxOne].x;
-    //                     placeOneY = placeList[idxOne].y;
-    //                     placeList[idxOne].x = placeList[i].x;
-    //                     placeList[idxOne].y = placeList[i].y;
-    //                     placeList[i].x = placeOneX;
-    //                     placeList[i].y = placeOneY;
-
-    //                     DrawField.drawTrack(trackList.trackList[0], placeList);
-    //                     clickone = true;
-    //                 }
-    //                 $('#outputTrackLength').text(Math.round(trackList.trackList[0].trackLength * 100) / 100);
-    //                 evt.target.graphics.clear().beginFill("#25ad0f").drawCircle(0, 0, 8);
-    //                 stage.update();
-    //             }
-    //         });
-    //     }
-    // }
 }
